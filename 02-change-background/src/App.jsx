7d5d66418905952c7097988cb0589718abbc9e8c
Jhon Lotero <jhoning.soft@gmail.com>
2024-02-08 19:47:55 -0500

    * Remove defaul App.css content * Add buttons that allow change the color of the background using the state of react.

--- 02-change-background/src/App.jsx
+++ 02-change-background/src/App.jsx
@@ -4,12 +4,28 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  /*
+    color is a state variable, if the value of color changes, the component will re-render (react propagates the changes to the DOM)
+    setColor is a function to change the value of color: see onClick={() => setColor('green')
+    olive is the initial value of color
+  */
+  const [color, setColor] = useState('olive')
 
   return (
-    <>
-      <h1 className='bg-green-500 text-3xl'>A back-ground changer with vite</h1>
-    </>
+
+    <div className='w-full h-screen duration-300' style={{backgroundColor:color}}>
+      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+        <button onClick={() => {setColor('red')}} className='outline-none px-4 py-1 rounded-full 
+          shadow-lg text-black' style={{backgroundColor: 'red'}}>Red</button>
+        
+        <button onClick={() => setColor('green')} className='outline-none px-4 py-1 rounded-full 
+          shadow-lg text-black' style={{backgroundColor: 'green'}}>Green</button>
+
+        <button onClick={() => setColor('blue')} className='outline-none px-4 py-1 rounded-full 
+          shadow-lg text-black' style={{backgroundColor: 'blue'}}>Blue</button>
+      </div>
+    </div>
   )
 }
 
